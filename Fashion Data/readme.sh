358508845893d4319ddfb0a3c3ae71faebac8b81
MBagasA <95059415+MBagasA@users.noreply.github.com>
2024-10-25 10:34:10 +0700

    Create readme.sh

---
+++ Fashion Data/readme.sh
@@ -0,0 +1,16 @@
+
+#SQL & TABLEAU INTEGRATION PROJECT
+
+##Fashion Data 
+
+
+welcome to my repository about fashion data, in this repository we display sales data, fashion trends in 2018-2023
+
+## Screenshots
+
+https://i.postimg.cc/7LyLNb8t/Dashboard-data.png
+## Tech Stack
+
+Postgree SQL
+EXCEL
+TABLEAU
